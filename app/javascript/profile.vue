06700ce361b66cae4edbd5192bb14f73d4410a75
Ayoub Belemlih <ayoub.bel.coding.exercise.1@gmail.com>
2021-05-27 09:43:16 -0400

    Add ability to link books directly from table of the author's books

--- app/javascript/profile.vue
+++ app/javascript/profile.vue
@@ -36,91 +36,134 @@
           <v-tab-item key="books" id="books">
             <v-container id="books-container">
               <v-row>
-                <v-data-table id="books-table"
-                    :headers="headers"
-                    :items="books"
-                    class="elevation-1"
+                <v-dialog
+                    v-model="dialog"
+                    persistent
+                    max-width="600px"
                 >
-                  <template v-slot:top>
-                    <v-toolbar
-                        flat
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        color="primary"
+                        dark
+                        v-bind="attrs"
+                        v-on="on"
                     >
-                      <v-toolbar-title>My Books</v-toolbar-title>
-                      <v-divider
-                          class="mx-4"
-                          inset
-                          vertical
-                      ></v-divider>
-                      <v-spacer></v-spacer>
-                      <v-dialog
-                          v-model="dialog"
-                          persistent
-                          max-width="600px"
-                      >
-                        <template v-slot:activator="{ on, attrs }">
-                          <v-btn
-                              color="primary"
-                              dark
-                              v-bind="attrs"
-                              v-on="on"
-                          >
-                            Add New Book
-                          </v-btn>
-                        </template>
-                        <v-card>
-                          <v-card-title>
-                            <span class="headline">New Book</span>
-                          </v-card-title>
-                          <v-card-text>
-                            <v-container>
-                              <v-row>
-                                <v-col cols="12">
-                                  <input type="file" id="file" ref="file" v-on:change="uploadCoverImage()"/>
-                                </v-col>
-                                <v-col cols="12">
-                                  <v-text-field
-                                      v-model="newBookISBN"
-                                      label="ISBN*"
-                                      required
-                                  ></v-text-field>
-                                </v-col>
-                                <v-col cols="12">
-                                  <v-text-field
-                                      v-model="newBookTitle"
-                                      label="Title*"
-                                      required
-                                  ></v-text-field>
-                                </v-col>
-                                <v-col cols="12">
-                                  <v-text-field
-                                      v-model="newBookPublisher"
-                                      label="Publisher*"
-                                      required
-                                  ></v-text-field>
-                                </v-col>
-                                <v-col cols="12">
-                                  <v-text-field
-                                      v-model="newBookPublicationYear"
-                                      label="Publication Year*"
-                                      required
-                                  ></v-text-field>
-                                </v-col>
-                                <v-col cols="12">
-                                  <v-text-field
-                                      v-model="newBookPublicationCountryCode"
-                                      label="Publication Country Code*"
-                                      required
-                                  ></v-text-field>
-                                </v-col>
-                              </v-row>
-                            </v-container>
-                            <small>*required field</small>
-                          </v-card-text>
-                        </v-card>
-                      </v-dialog>
-                    </v-toolbar>
+                      Add New Book
+                    </v-btn>
                   </template>
-                </v-data-table>
+                  <v-card>
+                    <v-card-title>
+                      <span class="headline">New Book</span>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12">
+                            <input type="file" id="file" ref="file" v-on:change="uploadCoverImage()"/>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-text-field
+                                v-model="newBookISBN"
+                                label="ISBN*"
+                                required
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-text-field
+                                v-model="newBookTitle"
+                                label="Title*"
+                                required
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-text-field
+                                v-model="newBookPublisher"
+                                label="Publisher*"
+                                required
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-text-field
+                                v-model="newBookPublicationYear"
+                                label="Publication Year*"
+                                required
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-text-field
+                                v-model="newBookPublicationCountryCode"
+                                label="Publication Country Code*"
+                                required
+                            ></v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                      <small>*required field</small>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                          color="red darken-1"
+                          text
+                          @click="dialog = false"
+                      >
+                        Close
+                      </v-btn>
+                      <v-btn
+                          color="blue darken-1"
+                          text
+                          @click="createBook"
+                      >
+                        Save
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-row>
+              <v-row>
+                <v-simple-table>
+                  <template v-slot:default>
+                    <thead>
+                    <tr>
+                      <th class="text-left">
+                        ID
+                      </th>
+                      <th class="text-left">
+                        ISBN
+                      </th>
+                      <th class="text-left">
+                        Title
+                      </th>
+                      <th class="text-left">
+                        Publisher
+                      </th>
+                      <th class="text-left">
+                        Publication Year
+                      </th>
+                      <th class="text-left">
+                        Publication Location
+                      </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr
+                        v-for="item in books"
+                        :key="item.id"
+                    >
+                      <td>
+                        <a target="_blank" :href="`localhost:3000/books/${item.id}`">
+                          {{ item.id }}
+                        </a>
+                      </td>
+                      <td>{{ item.isbn }}</td>
+                      <td>{{ item.title }}</td>
+                      <td>{{ item.publisher }}</td>
+                      <td>{{ item.publication_year }}</td>
+                      <td>{{ item.publication_country_code }}</td>
+                    </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
               </v-row>
             </v-container>
           </v-tab-item>
