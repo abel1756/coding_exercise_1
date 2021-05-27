2f2ce83327cbb0292b8bbe1227bf1c20ec32a5a4
Ayoub Belemlih <ayoub.bel.coding.exercise.1@gmail.com>
2021-05-27 00:29:03 -0400

    Add home page

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -7,9 +7,14 @@
 
 import Vue from 'vue'
 import App from '../app.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+    vuetify: new Vuetify(),
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
